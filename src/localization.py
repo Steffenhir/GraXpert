237d2ce463ec8d1aed35176eea85b5036b2342a9
Steffen Hirtle <hirtle@rhrk.uni-kl.de>
2022-04-15 15:00:30 +0200

    attempt to fix language detection

--- src/localization.py
+++ src/localization.py
@@ -4,13 +4,12 @@ import locale
 
 lang = None
 
-if sys.platform.startswith("win"):
+
     
-    lang, enc = locale.getdefaultlocale()
-    lang = gettext.translation('base', localedir='../locales', languages=[lang], fallback=True)
+lang, enc = locale.getdefaultlocale()
+lang = gettext.translation('base', localedir='../locales', languages=[lang], fallback=True)
     
-else:
-    lang = gettext.translation('base', localedir='../locales', fallback=True)
+
 
 lang.install()
 

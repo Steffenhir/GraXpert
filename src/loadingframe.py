9150778b784d96ca3621a5b2684275c7ace68931
David Schmelter <david.schmeltergooglemail.com>
2022-04-21 18:11:03 +0200

    force update of the 'Calculating' label to prevent white background on mac

--- src/loadingframe.py
+++ src/loadingframe.py
@@ -5,23 +5,28 @@ Created on Thu Mar 17 18:16:05 2022
 @author: Steffen
 """
 
-import tkinter as tk
+from tkinter import ttk
 from localization import _
 
 class LoadingFrame():
     def __init__(self, widget, toplevel):
 
         font = ('Verdana', 20, 'normal')
-      
 
         self.toplevel = toplevel
-        self.text = tk.Message(widget, text=_("Calculating..."), width=500 ,font=font)
-
+        self.text = ttk.Label(
+            widget, 
+            text=_("Calculating..."), 
+            font=font
+        )
         
     def start(self):
         
         self.text.pack(fill="none", expand=True)
         self.toplevel.update()
+        # force update of label to prevent white background on mac
+        self.text.configure(background="#313131")
+        self.text.update()
 
     def end(self):
         self.text.pack_forget()

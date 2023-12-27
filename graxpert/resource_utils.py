c6ea33eaa7ff326c90fc01a5e55833b71b5f52a0
David Schmelter <david.schmelter@googlemail.com>
2023-12-27 14:51:34 +0100

    fix calculation of resource_path

--- graxpert/resource_utils.py
+++ graxpert/resource_utils.py
@@ -1,22 +1,12 @@
 # from appdirs import
 import os
-import sys
 from tempfile import TemporaryDirectory
-
-import numpy as np
-from skimage import io
-from skimage.transform import resize
 
 temp_resource_dir = TemporaryDirectory()
 
 
 def resource_path(relative_path):
-    if getattr(sys, "frozen", False):
-        # The application is frozen
-        base_path = os.path.join(os.path.dirname(sys.executable), "lib")
-    else:
-        # The application is not frozen
-        base_path = os.path.join(os.path.dirname(__file__), "..")
+    base_path = os.path.join(os.path.dirname(os.path.dirname(__file__)))
     return os.path.join(base_path, relative_path)
 
 

bbb7663be74425ab3d83c5263c43a768aacc276b
Steffen Hirtle <hirtle@rhrk.uni-kl.de>
2024-03-30 15:32:36 +0100

    The median value near the mouse with a radius of 2 pixels is now shown in the right info text in the lower status bar

--- graxpert/astroimage.py
+++ graxpert/astroimage.py
@@ -208,21 +208,21 @@ class AstroImage:
         return
 
     def get_local_median(self, img_point):
-        sample_radius = 25
+        sample_radius = 2
         y1 = int(np.amax([img_point[1] - sample_radius, 0]))
         y2 = int(np.amin([img_point[1] + sample_radius, self.height]))
         x1 = int(np.amax([img_point[0] - sample_radius, 0]))
         x2 = int(np.amin([img_point[0] + sample_radius, self.width]))
 
         if self.img_array.shape[-1] == 3:
-            R = sigma_clipped_stats(data=self.img_array[y1:y2, x1:x2, 0], cenfunc="median", stdfunc="std", grow=4)[1]
-            G = sigma_clipped_stats(data=self.img_array[y1:y2, x1:x2, 1], cenfunc="median", stdfunc="std", grow=4)[1]
-            B = sigma_clipped_stats(data=self.img_array[y1:y2, x1:x2, 2], cenfunc="median", stdfunc="std", grow=4)[1]
+            R = np.median(self.img_array[y1:y2, x1:x2, 0])
+            G = np.median(self.img_array[y1:y2, x1:x2, 1])
+            B = np.median(self.img_array[y1:y2, x1:x2, 2])
 
             return [R, G, B]
 
         if self.img_array.shape[-1] == 1:
-            L = sigma_clipped_stats(data=self.img_array[x1:x2, y1:y2, 0], cenfunc="median", stdfunc="std", grow=4)[1]
+            L = np.median(self.img_array[x1:x2, y1:y2, 0])
 
             return L
 
